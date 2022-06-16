85c8cfe68bae3bae430028dfbce80e313a283472
Gorka Cano <gcano@websocialmanager.com>
2022-06-16 17:10:10 +0200

    feat: ejercicio tema 2

---
+++ 2.Sintaxis, variables y palabras reservadas de .../lists.js
@@ -0,0 +1,11 @@
+const birthdate = new Date('1984/05/20'); 
+const favoriteBook = {
+  title: 'Los pilares de la tierra',
+  author: 'Ken Follet',
+  date: new Date('1989'),
+  url: 'https://es.wikipedia.org/wiki/Los_pilares_de_la_Tierra',
+};
+
+const person =  ['Gorka',38, true, birthdate,favoriteBook];
+
+console.log(person);
