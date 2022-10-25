34027c0b7386a82bcd32ad85b7c9d72cfbb046c4
Gorka Cano <gcano@websocialmanager.com>
2022-10-26 00:43:30 +0200

    feat: lesson 7 exercise 1

---
+++ 7. Trabajando con Sets y Objetos/conjuntos.js
@@ -0,0 +1,8 @@
+const familyNames = ["Ana", "Miguel", "Aitor", "Gorka"];
+const uniqueFamilyNames = new Set(familyNames);
+console.log(uniqueFamilyNames);
+uniqueFamilyNames.add("Gorka");
+console.log(uniqueFamilyNames);
+console.log(uniqueFamilyNames);
+uniqueFamilyNames.add("Javascript");
+console.log(uniqueFamilyNames);
